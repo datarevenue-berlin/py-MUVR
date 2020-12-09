fb55e028f15642dcc0cda76b6180604c4a95bcda
Giulio Tirabassi <giulio@datarevenue.com>
2020-12-09 15:26:09 +0100

    Add dependencies to setup

--- setup.py
+++ setup.py
@@ -5,42 +5,53 @@
 import versioneer
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest', ]
+test_requirements = [
+    "pytest",
+]
 
 setup(
     author="Data Revenue GmbH",
-    author_email='giulio@datarevenue.com',
+    author_email="giulio@datarevenue.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Nested cross validation for feature selection in Python",
-    install_requires=requirements,
+    install_requires=[
+        "dask>=2.30.0",
+        "matplotlib>=3.3.2",
+        "numpy>=1.19.2",
+        "pandas>=1.1.3",
+        "scipy>=1.5.2",
+        "scikit-learn>=0.23.2",
+    ],
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='omigami',
-    name='omigami',
-    packages=find_packages(include=['omigami']),
+    keywords="omigami",
+    name="omigami",
+    packages=find_packages(include=["omigami"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/datarevenue-berlin/omigami',
-    version = versioneer.get_version(),
-    cmdclass = versioneer.get_cmdclass(),
+    url="https://github.com/datarevenue-berlin/omigami",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
 )
