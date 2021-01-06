ebc7c36f6930ce294c2564f56c657c12872f5e13
Giulio Tirabassi <giulio@datarevenue.com>
2021-01-06 16:57:28 +0100

    update feature evaluator with new model

--- omigami/feature_evaluator.py
+++ omigami/feature_evaluator.py
@@ -5,6 +5,7 @@ import numpy as np
 from omigami.types import Estimator, MetricFunction, RandomState, NumpyArray
 from omigami.models import InputData, FeatureEvaluationResults, FeatureRanks
 from omigami.estimator import ModelTrainer
+from omigami.model import make_estimator
 
 
 class FeatureEvaluator:
@@ -15,7 +16,7 @@ class FeatureEvaluator:
         metric: Union[str, MetricFunction],
         random_state: Union[int, RandomState],
     ):
-        self._model_trainer = ModelTrainer(estimator, random_state=random_state)
+        self._model_trainer = make_estimator(estimator, random_state)
         self._metric = self._make_metric(metric)
         self._random_state = random_state
         self._n_features = input_data.X.shape[1]
@@ -30,8 +31,8 @@ class FeatureEvaluator:
         y_pred = estimator.predict(X_test)
 
         score = -self._metric(y_test, y_pred)
-        feature_ranks = self._get_feature_ranks(estimator, features)
-        return FeatureEvaluationResults(test_score=score, ranks=feature_ranks)
+        ranks = self._get_feature_ranks(estimator, features)
+        return FeatureEvaluationResults(test_score=score, ranks=ranks, model=estimator)
 
     def get_n_features(self):
         return self._n_features
@@ -56,25 +57,10 @@ class FeatureEvaluator:
             return get_scorer(metric_string)._score_func
         raise ValueError("Input metric is not a valid string")
 
-    def _get_feature_importances(self, estimator: Estimator):
-        if hasattr(estimator, "feature_importances_"):
-            return estimator.feature_importances_
-        elif hasattr(estimator, "coef_"):
-            return np.abs(estimator.coef_[0])
-        elif hasattr(estimator, "steps"):
-            for _, step in estimator.steps:
-                if hasattr(step, "coef_") or hasattr(step, "feature_importances_"):
-                    return self._get_feature_importances(step)
-        else:
-            raise ValueError("The estimator provided has no feature importances")
-
     def _get_feature_ranks(
         self, estimator: Estimator, features: Iterable[int]
     ) -> FeatureRanks:
-        """Extract the feature rank from the input estimator. So far it can only handle
-        estimators as scikit-learn ones, so either having the `feature_importances_` or
-        the `coef_` attribute."""
-        feature_importances = self._get_feature_importances(estimator)
+        feature_importances = estimator.feature_importances
         ranks = rankdata(-feature_importances)
         return FeatureRanks(features=features, ranks=ranks, n_feats=self._n_features)
 
