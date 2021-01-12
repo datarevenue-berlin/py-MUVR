c7e59a17d07c4a1b2016e99763eb62eb9a27e9f3
carlos <carlos@datarevenue.com>
2021-01-12 10:29:45 +0100

    Add more tests to data slice and docstrings to feature_evaluator.py

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -5,3 +5,5 @@ def test_n_features(dataset):
 def test_input_data_slice(dataset):
     assert dataset[:5, 3:7].X.shape == (5, 4)
     assert dataset[[1,2,5], [3,4,7]].X.shape == (3, 3)
+    assert dataset[1:3, :].X.shape == (2, 12)
+    assert dataset[:,:].X.shape == dataset.X.shape
