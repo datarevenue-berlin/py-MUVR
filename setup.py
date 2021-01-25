b737ef766d9d2fa2b811866a17ea6a86e45ff24c
Giulio Tirabassi <giulio@datarevenue.com>
2021-01-25 16:04:13 +0100

    exclude tests

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     include_package_data=True,
     keywords="omigami",
     name="omigami",
-    packages=find_packages(include=["omigami"]),
+    packages=find_packages(exclude=["tests"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
