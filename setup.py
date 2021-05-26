33be39faf3bbe43885243d2439725350c54107f6
czanella <carlos@datarevenue.com>
2021-05-26 10:38:04 +0200

    Update url name with proper casing
    #time 4m

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/datarevenue-berlin/pymuvr",
+    url="https://github.com/datarevenue-berlin/pyMUVR",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
